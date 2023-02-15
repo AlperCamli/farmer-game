0af722ec750824a18b7b65af5bd0d5bffa942ca9
AlperCamli <camlialper03@gmail.com>
2023-02-15 08:14:38 +0300

    mac v1

---
+++ Assets/FruitPrefab.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitPrefab : MonoBehaviour
+{
+
+    [SerializeField] public float speed = 1f; // The speed at which the object moves
+
+
+
+    public void MoveToTarget(Transform target)
+    {
+
+
+        // Calculate the new position of the object
+        Vector3 newPosition = Vector3.Lerp(transform.position, target.position, speed * Time.deltaTime);
+
+        // Update the position of the object
+        transform.position = newPosition;
+    }
+}
+
